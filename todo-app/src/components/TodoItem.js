3b31c7cdd4a98f91c377ca867a4bbcd7e35c3e22
Krithic Kumar <krithickumarub@protonmail.com>
2023-08-05 22:14:54 +0530

    add todo list css and update js file

--- todo-app/src/components/TodoItem.js
+++ todo-app/src/components/TodoItem.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
+import "./TodoItem.css";
 
 class TodoItem extends Component {
 	render() {
 		const item = this.props.itemData;
 		return (
-			<div key={item.id}>
-				<h3> {item.title} </h3>
-				<h3> {item.status} </h3>
+			<div className="flex-container" key={item.id}>
+				<div className="flex-child">
+					<h3> {item.title} </h3>
+				</div>
+				<div className="flex-child">
+					<h3> {item.status} </h3>
+				</div>
 			</div>
 		);
 	}
