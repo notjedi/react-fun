fd51fec1c1690968c6ea700b4508f5820fbc03bd
Krithic Kumar <krithickumarub@protonmail.com>
2023-08-12 22:58:48 +0530

    move state to App.js

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,21 +2,64 @@ import "./App.css";
 import TodoList from "./components/TodoList.js";
 import Button from "./components/Button.js";
 import Popup from "./components/Popup";
-import { useState } from "react";
+// import { useState } from "react";
+import { Component } from "react";
 
-function App() {
-	const [popupOpen, setPopupOpen] = useState(false);
+class App extends Component {
+	state = {
+		popupOpen: false,
+		items: [
+			{
+				id: 0,
+				title: "Test",
+				status: "completed",
+			},
+			{
+				id: 1,
+				title: "Go piss",
+				status: "incomplete",
+			},
+		],
+	};
 
-	return (
-		<div className="App">
-			<br />
-			<br />
-			<TodoList />
-			<Popup popupOpen={popupOpen} setPopupOpen={setPopupOpen} />
-			<br />
-			<Button title="Add Task" onClick={() => setPopupOpen(!popupOpen)} />
-		</div>
-	);
+	setPopupOpen = () => {
+		this.setState({
+			popupOpen: !this.state.popupOpen,
+		});
+	};
+
+	addTodo = (item) => {
+		this.state.items.push(item);
+		this.setState({
+			items: [...this.state.items],
+		});
+	};
+
+	removeTodo = (id) => {
+		this.setState({
+			items: this.state.items.filter((object) => object.id !== id),
+		});
+	};
+
+	render() {
+		// const [popupOpen, setPopupOpen] = useState(false);
+		const { popupOpen } = this.state;
+
+		return (
+			<div className="App">
+				<br />
+				<br />
+				<TodoList itemData={this.state.items} onClickDelete={this.removeTodo} />
+				<Popup
+					popupOpen={popupOpen}
+					setPopupOpen={this.setPopupOpen}
+					onClickAdd={this.addTodo}
+				/>
+				<br />
+				<Button title="Add Task" onClick={this.setPopupOpen} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,36 +2,16 @@ import React, { Component } from "react";
 import TodoItem from "./TodoItem.js";
 
 class TodoList extends Component {
-	state = {
-		items: [
-			{
-				id: 0,
-				title: "Test",
-				status: "completed",
-			},
-			{
-				id: 1,
-				title: "Go piss",
-				status: "incomplete",
-			},
-		],
-	};
-
-	removeTodo = (id) => {
-		this.setState({
-			items: this.state.items.filter((object) => object.id !== id),
-		});
-	};
-
 	render() {
-		const { items } = this.state;
+		const items = this.props.itemData;
+		const { onClickDelete } = this.props;
 
 		return (
 			<div className="list">
 				{items && items.length > 0 ? (
 					items.map((item) => (
 						<div key={item.id}>
-							<TodoItem itemData={item} onClickDelete={this.removeTodo} />
+							<TodoItem itemData={item} onClickDelete={onClickDelete} />
 							<br />
 						</div>
 					))
